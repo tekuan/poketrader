f16ff8bd2afa9961670289a9c43c1fcc2499391f
Larissa Bianca <larissa_bianca@outlook.com>
2021-11-16 01:56:54 -0300

    refactor handleRightSide

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -22,21 +22,21 @@ export function Card({ url, leftSide, setLeftSide, rightSide, setRightSide }) {
       : setLeftSide((prev) => [...prev, pokemon]);
   };
 
+  const handleRightSide = () => {
+    rightSide.length === 6
+      ? alert("Selecionar no máximo 6 pokémons!")
+      : setRightSide((prev) => [...prev, pokemon]);
+  };
+
   return (
     <PokeCard>
+      <p>XP {pokemon?.base_experience} </p>
       <div>
         <button style={{ color: "#0086ff" }} onClick={handleLeftSide}>
           <FaAngleLeft size={30} />
         </button>
         <img src={pokemon?.sprites?.front_default} alt="pokemon" />
-        <button
-          style={{ color: "#ff1a1a" }}
-          onClick={() => {
-            rightSide.length === 6
-              ? alert("Selecionar no máximo 6 pokémons!")
-              : setRightSide((prev) => [...prev, pokemon]);
-          }}
-        >
+        <button style={{ color: "#ff1a1a" }} onClick={handleRightSide}>
           <FaAngleRight size={30} />
         </button>
       </div>
