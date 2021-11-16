436227c8bdb75838596b14f2d9e33fac2923ba90
Larissa Bianca <larissa_bianca@outlook.com>
2021-11-16 14:20:09 -0300

    remove

--- src/Components/Alert/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Notification } from "./style";
-export function Alert() {
-  console.log(" :>dandasbdj> ");
-  return <Notification>Larisssa</Notification>;
-}
--- src/Components/Alert/style.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const Notification = styled.div`
-  display: flex;
-`;
